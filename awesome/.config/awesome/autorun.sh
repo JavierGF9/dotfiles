2e42961e94ee4ceec4d7a4f24951fc8ba9359f5d
Javier Gómez Fernández <javigofe@gmail.com>
2018-02-07 13:13:39 +0100

    Added awesome wm configuration

---
+++ awesome/.config/awesome/autorun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1
+run nm-applet
+run conky -d
+run urxvtd -q -o -f
+run compton -b
