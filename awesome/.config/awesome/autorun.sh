a9807c0cb1c1ba9d28f921804d4553331bddf51b
Javier Gómez Fernández <javigofe@gmail.com>
2018-02-07 19:14:42 +0100

    Added more bindings and screen locker to Awesome WM

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -11,4 +11,4 @@ run /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1
 run nm-applet
 run conky -d
 run urxvtd -q -o -f
-run compton -b
+run compton --config /dev/null -b --backend glx --vsync opengl-swc
