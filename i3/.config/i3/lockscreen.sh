912b2aed4ff1dd53f9e9c60ec43c2fede7f8c5a7
Javier Gómez Fernández <javigofe@gmail.com>
2018-05-22 08:38:04 +0200

    [i3] Added new keybindings

--- i3/.config/i3/lockscreen.sh
+++ i3/.config/i3/lockscreen.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+revert_dpms() {
+	xset +dpms dpms 600 600 600
+}
+
 # suspend message display
 pkill -u "$USER" -USR1 dunst
 
+trap revert_dpms SIGHUP SIGINT SIGTERM
+xset +dpms dpms 5 5 5
 # lock the screen
 i3lock -n -c 000000 -e
+revert_dpms
 
 # resume message display
 pkill -u "$USER" -USR2 dunst
