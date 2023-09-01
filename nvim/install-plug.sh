3c88d2e85d1510128074a10d1a3667b0b104e2cf
Javier Gómez Fernández <javigofe@gmail.com>
2023-09-01 22:12:26 +0200

    [nvim] better vim-plug installer

--- nvim/install-plug.sh
+++ nvim/install-plug.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm -f ~/.local/share/nvim/site/autoload/plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
